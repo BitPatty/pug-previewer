77f88fa69d9ffa38ff01fb67cffac55977b47f5f
Matteias Collet <matteias.collet@bluewin.ch>
2022-12-05 01:29:28 +0000

    add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,14 @@ import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
   return (
     <>
+      <Head>
+        <title>Pug Previewer</title>
+      </Head>
       <main className="p-5">
         <Component {...pageProps} />
       </main>
